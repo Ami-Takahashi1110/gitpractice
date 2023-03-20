68291d5120ef3c442a9db4997a0517591d759e3b
Ami-Takahashi1110 <ami.takahashi1110@gmail.com>
2023-03-20 16:28:06 +0900

    タスク管理アプリrealmエラーのコミット

---
+++ MyTask/MyTask/UserTable.swift
@@ -0,0 +1,21 @@
+//
+//  UserTable.swift
+//  MyTask
+//
+//  Created by USER on 2023/03/20.
+//
+
+import Foundation
+import RealmSwift
+
+class UserTable: object {
+    // 日付
+    
+    
+    // タイトル
+    
+    // 内容
+    
+    // カテゴリー
+    
+}
