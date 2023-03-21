280b915cbd98f46123671687d153220416941f1e
Ami-Takahashi1110 <ami.takahashi1110@gmail.com>
2023-03-21 09:32:27 +0900

    CRUD処理の試し書きのコミット

--- MyTask/MyTask/UserTable.swift
+++ MyTask/MyTask/UserTable.swift
@@ -8,14 +8,55 @@
 import Foundation
 import RealmSwift
 
-class UserTable: object {
+class UserTable: Object {
+    // インプット項目の定義
     // 日付
-    
-    
+    @objc dynamic var date: Date = Date()
     // タイトル
-    
+    @objc dynamic var title: String = ""
     // 内容
-    
+    @objc dynamic var contents: String = ""
     // カテゴリー
+    @objc dynamic var category: String = ""
+
+    // 前画面で入力された値を取得する方法はあとで調べる
+    // 入力処理
+    func input() {
+        let userTable = UserTable()
+        userTable.date = Date(timeIntervalSince1970: 631152000) // 呼び出し方を考える
+        userTable.title = "aaa" // 呼び出し方を考える
+        userTable.contents = "bbb" // 呼び出し方を考える
+        userTable.category = "ccc" // 呼び出し方を考える
+        
+        let realm = try! Realm()
+        try! realm.write {
+            realm.add(userTable)
+        }
+    }
+        
+    // 更新処理
+    func update() {
+        let realm = try! Realm()
+        let userTable = realm.objects(UserTable.self).first // 最初のオブジェクトを取得する
+        // 内容を更新する
+        try! realm.write {
+            userTable?.contents = "ddd" // オブジェクトが見つからない場合を考えてオプショナル型にする
+        }
+    }
+        
+    // 削除処理
+    func delete() {
+        let realm = try! Realm()
+        // 最初のオブジェクトを取得して削除する
+        guard let userTable = realm.objects(UserTable.self).first else {
+            return // オブジェクトが存在しない場合に処理を中止する
+        }
+        try! realm.write {
+            realm.delete(userTable)
+        }
+    }
     
+    
+
 }
+
--- MyTask/MyTask/ViewController.swift
+++ MyTask/MyTask/ViewController.swift
@@ -8,7 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
+    @IBOutlet weak var searchText: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    // 検索ボタンをクリック時
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        // キーボードを閉じる
+        view.endEditing(true)
+        
+        if let searchWord = searchBar.text {
+            // デバッグエリアに出力
+            print(searchWord)
+            // 入力されたカテゴリを検索
+            searchCategory(keyword: searchWord)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
